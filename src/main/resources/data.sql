6ddd81e17506917cce531bb1ef0472613500b7df
fernando-bertolo <fernando.bertolo.jr123@gmail.com>
2025-04-10 23:27:54 -0300

    feat: Adicionando tabela aluguel

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,8 +16,20 @@ CREATE TABLE pessoas (
      email VARCHAR(255)
 );
 
+CREATE TABLE alugueis (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    pessoa_id BIGINT NOT NULL,
+    veiculo_id BIGINT NOT NULL,
+    data_inicio DATE,
+    data_fim DATE,
+    valor_total DECIMAL(10,2),
+    FOREIGN KEY (pessoa_id) REFERENCES pessoas(id),
+    FOREIGN KEY (veiculo_id) REFERENCES veiculos(id)
+);
+
 
 
 
 INSERT INTO veiculos (marca, modelo, placa, ano, cor, valor_diaria) VALUES ('Chevrolet', 'Celta', 'ABC-123', 2010, 'Preta', 100);
 INSERT INTO pessoas (nome, cpf, telefone, email) VALUES ('Fernando', '98127312', '19 999999', 'fernando@gmail.com');
+INSERT INTO alugueis (pessoa_id, veiculo_id, data_inicio, data_fim, valor_total) VALUES (1, 1, '2025-10-1', '2025-10-15', 1500.00);
